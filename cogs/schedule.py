1dd0de869af39130a6a943c43a43d098e67cc242
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-06-06 16:19:58 +0300

    Added proper scheduler, announces event starts

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1,8 +1,10 @@
-from discord.ext import tasks, commands
+from discord.ext import commands
 import xml.etree.ElementTree as ET
 import urllib.request
 from datetime import datetime as dt
 from datetime import timedelta
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 SCHEDULE_LINK = "https://events.opensuse.org/conferences/oSC22/schedule.xml"
 # SCHEDULE_LINK = "https://suno.pona.la/conferences/2022/schedule.xml"
@@ -10,39 +12,37 @@ SCHEDULE_LINK = "https://events.opensuse.org/conferences/oSC22/schedule.xml"
 def xml_from_https(link):
     return ET.fromstring(urllib.request.urlopen(link).read().decode("utf8"))
 
+def schedule_from_xml(xml):
+    schedule = []
+    for room_xml in xml.findall("./day/room"):
+        room = []
+        for event_xml in room_xml.findall("./event"):
+            room.append(Event(event_xml, room))
+        # This step has to happen after room is filled
+        for event in room:
+            event.initialize_adjacent_events()
+        schedule.extend(room)
+    return schedule
+
 class CogSchedule(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.tree = xml_from_https(SCHEDULE_LINK)
-        self.schedule = self.build_schedule()
-        self.printer.start()
+        self.schedule = schedule_from_xml(xml_from_https(SCHEDULE_LINK))
+        self.scheduler = AsyncIOScheduler()
+        self.schedule_events()
+        self.print_schedule()
 
-    def cog_unload(self):
-        self.printer.cancel()
-
-    @tasks.loop(seconds=5.0)
-    async def printer(self):
+    def schedule_events(self):
         for event in self.schedule:
-            print(event.title)
-            print(f"room: {event.room}")
-            print(f"starts: {event.start}")
-            print(f"previous: {event.prev}")
-            print(f"next: {event.next}")
-            print("="*60)
-        #for timestamp, event in self.events.items():
-        #    print(get_event_status(event), event.find("title").text)
+            self.scheduler.add_job(self.announce_event_start,
+                                   "date", run_date=event.start,
+                                   args=[event])
+    def print_schedule(self):
+        for event in self.schedule:
+            event.announce()
 
-    def build_schedule(self):
-        schedule = []
-        for room_xml in self.tree.findall("./day/room"):
-            room = []
-            for event_xml in room_xml.findall("./event"):
-                room.append(Event(event_xml, room))
-            # This step has to happen after room is filled
-            for event in room:
-                event.initialize_adjacent_events()
-            schedule.extend(room)
-        return schedule
+    def announce_event_start(self, event):
+        event.announce()
 
 
 def duration_timedelta(timestamp):
@@ -54,7 +54,7 @@ def duration_timedelta(timestamp):
 def now():
     # Helper function for testing purposes.
     # Allows the "now" to travel in time.
-    return dt.now() - timedelta(days=2, hours=3, minutes=30)
+    return dt.now() - timedelta(days=2, hours=6)
 
 
 class Event():
@@ -84,7 +84,7 @@ class Event():
             return None
         return self.room_events[self.room_events.index(self) - 1]
 
-    def get_event_status(self):
+    def status(self):
         # negative: has not yet happened
         # positive: has happened
         time_to_event_start = now() - self.start
@@ -95,3 +95,12 @@ class Event():
         if time_to_event_end.total_seconds() < 0:
             return "ongoing"
         return "finished"
+
+    def announce(self):
+        print(self.title)
+        print(f"room: {self.room}")
+        print(f"status: {self.status()}")
+        print(f"starts: {self.start}")
+        print(f"previous: {self.prev}")
+        print(f"next: {self.next}")
+        print("="*60)
